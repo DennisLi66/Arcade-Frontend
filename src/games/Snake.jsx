31cd8108cc7d236732b25bd8bcf090d63920485e
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-12-28 06:55:52 -0800

    Created More Visually Appealing Gameboard

--- src/games/Snake.jsx
+++ src/games/Snake.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from 'react-bootstrap/Button'
+import "./css/Snake.css"
 
 function Snake() {
   //IMPORTANT GAME VARIABLES
@@ -16,7 +17,7 @@ function Snake() {
     gameBoard.push(borderRow);
     //push middle content
     var row = [];
-    row.push("X")
+    row.push("X");
     for (let i = 0; i < 40; i++){
       row.push("0");
     }
@@ -26,23 +27,40 @@ function Snake() {
     }
     //push bottom border row
     gameBoard.push(borderRow);
-    console.log(gameBoard);
+    // console.log(gameBoard);
   }
   function readInstructions(){
-
+    document.getElementById("snakeBoard").innerHTML = (
+      <div>
+        <Button onClick={getFrontPage}>Back</Button>
+        <h1> Instructions </h1>
+      </div>
+    )
   }
   function printSnakeBoard(){
-    var toPrint = "";
+    var toPrint = "<h1>Snake</h1><div class='gameBoard'>";
     for (let row = 0; row < 42; row++){
       for (let col = 0; col < 42; col++){
-        toPrint += gameBoard[row][col];
+        // toPrint += gameBoard[row][col];
+        toPrint += "<div class='boardSquare'>" // + (row * 42 + col)
+         + "</div>"
       }
       toPrint += "<br></br>"
     }
-    document.getElementById("snakeBoard").innerHTML = toPrint;
+    document.getElementById("snakeBoard").innerHTML = toPrint + "</div>";
   }
   function startSnakeGame(){
     printSnakeBoard();
+  }
+  function getFrontPage(){
+    document.getElementById("snakeBoard").innerHTML =
+    (
+      <div>
+        <h1>Snake</h1>
+        <Button onClick={startSnakeGame}>Play Snake</Button><br></br>
+        <Button onClick={readInstructions}>Read Instructions</Button><br></br>
+      </div>
+    )
   }
 
   fillGameBoard();
