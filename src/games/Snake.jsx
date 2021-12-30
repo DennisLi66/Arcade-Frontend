23735add36b660b5934e91c76cdd9c87856a9d1c
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2021-12-29 18:46:28 -0800

    Simple Score Bar Added

--- src/games/Snake.jsx
+++ src/games/Snake.jsx
@@ -2,12 +2,20 @@ import React from "react";
 import Button from 'react-bootstrap/Button'
 import "./css/Snake.css"
 
+//To do
+//convert to a react hook
+//Draw snake
+//write function to spawn targets
 function Snake() {
   //IMPORTANT GAME VARIABLES
   var gameBoard = []; //    42 * 42 Board
+  var score = 0;
+  var gameStarted = false;
   //var direction;
   //var inMotion;
   //var snakeLength;
+  //var snakePositions; //is a stack
+
   function fillGameBoard(){
     var borderRow = []
     for (let i = 0; i < 42; i++){
@@ -30,30 +38,48 @@ function Snake() {
     // console.log(gameBoard);
   }
   function readInstructions(){
-    document.getElementById("snakeBoard").innerHTML = (
+    document.getElementById("gameScreen").innerHTML = (
       <div>
         <Button onClick={getFrontPage}>Back</Button>
         <h1> Instructions </h1>
       </div>
     )
   }
+  //Printers
   function printSnakeBoard(){
-    var toPrint = "<h1>Snake</h1><div class='gameBoard'>";
+    var toPrint = "<h1>Snake</h1><div class='gameBoard' id='gameBoard'>";
     for (let row = 0; row < 42; row++){
       for (let col = 0; col < 42; col++){
         // toPrint += gameBoard[row][col];
-        toPrint += "<div class='boardSquare'>" // + (row * 42 + col)
-         + "</div>"
+        if (gameBoard[row][col] === '0'){
+          toPrint += "<div class='boardSquare'>" // + (row * 42 + col)
+           + "</div>"
+        }else if (gameBoard[row][col] === 'X'){
+          toPrint += "<div class='borderSquare'>" // + (row * 42 + col)
+           + "</div>"
+        }
       }
       toPrint += "<br></br>"
     }
-    document.getElementById("snakeBoard").innerHTML = toPrint + "</div>";
+    toPrint += "</div>";
+    toPrint += "<div class='bulletinBoard' id='bulletinBoard'></div>";
+    document.getElementById("gameScreen").innerHTML = toPrint;
+    printBulletinBoard();
+  }
+  function printBulletinBoard(){
+    var text = "<Button onClick=''>Return</Button>";
+    if (gameStarted){
+      text += " Score: " + score;
+    }else{
+      text += " Press on any of the arrow keys to start."
+    }
+    document.getElementById("bulletinBoard").innerHTML = text;
   }
   function startSnakeGame(){
     printSnakeBoard();
   }
   function getFrontPage(){
-    document.getElementById("snakeBoard").innerHTML =
+    document.getElementById("gameScreen").innerHTML =
     (
       <div>
         <h1>Snake</h1>
@@ -65,7 +91,7 @@ function Snake() {
 
   fillGameBoard();
   return (
-    <div className="snakeBoard" id="snakeBoard">
+    <div className="gameScreen" id="gameScreen">
       <h1>Snake</h1>
       <Button onClick={startSnakeGame}>Play Snake</Button><br></br>
       <Button onClick={readInstructions}>Read Instructions</Button><br></br>
