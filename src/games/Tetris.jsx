151e4ad4386791d1db14f6f0d90f724ffecdd814
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2022-01-16 04:40:57 -0800

    Update Tetris.jsx
    Basic Front Page Function and expected variables added

--- src/games/Tetris.jsx
+++ src/games/Tetris.jsx
@@ -1,19 +1,47 @@
 import React from "react";
 import Button from 'react-bootstrap/Button'
-//import ReactDOMServer from 'react-dom/server';
+import ReactDOMServer from 'react-dom/server';
 require('dotenv').config();
 
 
 function Tetris(){
   var gameBoard = [];
+  var score = 0;
+  var startingTime = 0;
+  var intervalID = "";
+  var direction = false; //will also tell us if gamestarted
+  var endingTime = 0;
+  var timeTilDescent = 0;
+  function setBoard(){
+
+  }
   function startGame(){
 
   }
   function readInstructions(){
+    document.getElementById('gameScreen').innerHTML = ReactDOMServer.renderToStaticMarkup(
+      <div>
+        <Button id='backButton'> Back </Button><br></br>
+        <h1> Instructions </h1>
+      </div>
+    )
+    document.getElementById("backButton").onclick = getFrontPage();
+  }
+  function getScoresPage(message = "", rule = "", results = [], start = 0, end = 10){
 
   }
-  function getScoresPage(){
-
+  function getFrontPage(){
+    document.getElementById('gameScreen').innerHTML = ReactDOMServer.renderToStaticMarkup(
+      <div className='gameScreen' id='gameScreen'>
+        <h1> Tetris </h1>
+        <Button id='startGameButton' >Start Game</Button><br></br>
+        <Button id='instructionsButton'>Read Instructions</Button><br></br>
+        <Button id='scoresButton'>Scores</Button><br></br>
+      </div>
+    );
+      document.getElementById("startGameButton").onclick = startGame();
+      document.getElementById("instructionsButton").onclick = readInstructions();
+      document.getElementById("scoresButton").onclick = getScoresPage();
   }
 
   return (
@@ -22,14 +50,6 @@ function Tetris(){
       <Button onClick={()=>{startGame()}}>Start Game</Button><br></br>
       <Button onClick={()=>{readInstructions()}}>Read Instructions</Button><br></br>
       <Button onClick={()=>{getScoresPage()}}>Scores</Button><br></br>
-
-
-
-
-
-
-
-
     </div>
   )
 }
