2aefb14d62c535a3f4e573dd9fcdbeee29cf438e
Dennis Li <46885316+DennisLi66@users.noreply.github.com>
2022-03-01 06:05:17 -0800

    Board Updated with mine proximity

--- src/games/MineSweeper.jsx
+++ src/games/MineSweeper.jsx
@@ -9,6 +9,7 @@ require('dotenv').config();
 
 function MineSweeper(){
   var minesweeperBoard = []; //14 high by 18 wide
+  var revealedBoard = [];
   const mines = 40;
   var startingTime, endingTime = 0;
   var flagsPlaced = 0;
@@ -16,15 +17,30 @@ function MineSweeper(){
   function setBoard(){//randomly place mines
     var randomMines = [];
     minesweeperBoard = [];
+    revealedBoard = [];
     flagsPlaced = 0;
     for (let i = 0; i < 14*18; i++){
       randomMines.push(i);
       minesweeperBoard.push(0);
+      revealedBoard.push(0);
     }
     //scramble randomMines and pick the first 40;
     randomMines = fisherYatesShuffle(randomMines);
     for (let i = 0; i < mines; i++){
-      minesweeperBoard[randomMines.shift()] = 1;
+      minesweeperBoard[randomMines.shift()] = -1;
+    }
+    //FIX THIS: Change Numbers to proximity to mines
+    for (let i = 0; i < minesweeperBoard.length; i++){
+      if (minesweeperBoard[i] === -1){
+        if (i - 18 >= 0) minesweeperBoard[i - 18] = minesweeperBoard[i - 18] + 1 //Above
+        if (i + 18 < 14*18) minesweeperBoard[i + 18] = minesweeperBoard[i + 18] + 1 //Below
+        if (i % 18 !== 0) minesweeperBoard[i - 1] = minesweeperBoard[i - 1] + 1; //Left
+        if (i + 1 % 18 !== 0) minesweeperBoard[i + 1] = minesweeperBoard[i + 1] + 1; //Right
+        if ( (i % 18 !== 0) && i - 19 >= 0) minesweeperBoard[i - 19] = minesweeperBoard[i - 19] + 1; //Above Left
+        if ( (i % 18 !== 0) && (i + 17 < 14*18)) minesweeperBoard[i + 17] = minesweeperBoard[i + 17] + 1 //Below Left
+        if ( (i + 1 % 18 !== 0) && i - 17 >= 0) minesweeperBoard[i - 17] = minesweeperBoard[i - 17] + 1; //Above Right
+        if ( (i + 1 % 18 !== 0) && (i + 19 < 14*18)) minesweeperBoard[i + 19] = minesweeperBoard[i + 19] + 1; //Below Right
+      }
     }
     document.getElementById('gameScreen').innerHTML = ReactDOMServer.renderToStaticMarkup(
       <div className='mineSweeperStack'>
@@ -40,9 +56,11 @@ function MineSweeper(){
   }
   function printMineSweeperBoard(){
     //      minesweeperBoard.push(<div className='mineSweeperUnknownSquare' id={'square' + i} key={i}></div>)
-    // for (let i = 0; i < minesweeperBoard.length; i++){
-    //
-    // }
+    for (let i = 0; i < minesweeperBoard.length; i++){
+      // if (){
+      //
+      // }
+    }
   }
   function printMineSweeperScoreBoard(){
     document.getElementById('mineSweeperScoreBoard').innerHTML = ReactDOMServer.renderToStaticMarkup(
